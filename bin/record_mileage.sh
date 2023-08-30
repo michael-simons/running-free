5addc986aa817179b94efe2c5a2001aaa66966fb
Michael J. Simons <michael@simons.ac>
2023-08-30 07:41:33 +0200

    Prevent smaller values from being inserted.

--- bin/record_mileage.sh
+++ bin/record_mileage.sh
@@ -9,12 +9,14 @@ MILEAGE="$3"
 
 duckdb "$DB" -s "
   WITH hlp AS (
-    SELECT max(recorded_on) + INTERVAL 1 MONTH AS recorded_on, m.bike_id
+    SELECT max(recorded_on) + INTERVAL 1 MONTH AS recorded_on, m.bike_id, last(amount) AS last_amount
     FROM milages m JOIN bikes b ON b.id = m.bike_id
-    WHERE b.name = '$BIKE'
+    WHERE b.name = 'Open'
     GROUP BY ALL
+    ORDER BY ALL
   )
   INSERT INTO milages BY NAME
-  SELECT hlp.*, $MILEAGE AS amount, date_trunc('second', now()) AS created_at
+  SELECT hlp.* EXCLUDE(last_amount), $MILEAGE AS amount, date_trunc('second', now()) AS created_at
   FROM hlp
+  WHERE hlp.last_amount <= $MILEAGE
 "
